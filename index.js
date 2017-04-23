e5f7667b4007ed27479c43810d8f12f9e004bbad
Mark Mendoza <markolo25@gmail.com>
2017-04-22 23:57:14 -0700

    WE GOT JASON

---
+++ index.js
@@ -0,0 +1,4 @@
+function myFunc(ID){
+           var x = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/" + ID + " frameborder=\"0\" allowfullscreen></iframe>";
+           return x;
+       }
